6c592a696ef85f8200ab40a4b41cf79c31d3f473
Madsanka <mwattale@gmail.com>
2022-05-08 12:06:45 +0530

    Initial project structure

---
+++ src/test/java/com/sliitreg_33/hotelreservationsystem/HotelReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliitreg_33.hotelreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
