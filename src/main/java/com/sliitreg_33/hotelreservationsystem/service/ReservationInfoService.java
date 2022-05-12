c1ee93798dc83782785d16705213886f66a0fe1a
Madusanka-w <mwattale@gmail.com>
2022-05-12 07:32:16 +0530

    implement reservation information service

--- src/main/java/com/sliitreg_33/hotelreservationsystem/service/ReservationInfoService.java
+++ src/main/java/com/sliitreg_33/hotelreservationsystem/service/ReservationInfoService.java
@@ -1,4 +1,25 @@
 package com.sliitreg_33.hotelreservationsystem.service;
 
+import com.sliitreg_33.hotelreservationsystem.model.ReservationInfo;
+
+import java.util.List;
+
 public interface ReservationInfoService {
+
+    //reservation info
+    ReservationInfo addReservationInfo(ReservationInfo reservationInfo);
+
+    //get all reservation info
+    List<ReservationInfo> getAllReservation();
+
+    //get reservation info by id
+    ReservationInfo getReservationInfoById(int id);
+
+    //update reservation info
+
+
+    //delete reservation info
+    void deleteReservationInfo(int id);
+
 }
+
--- src/main/java/com/sliitreg_33/hotelreservationsystem/service/impl/ReservationInfoImpl.java
+++ src/main/java/com/sliitreg_33/hotelreservationsystem/service/impl/ReservationInfoImpl.java
@@ -1,4 +1,28 @@
 package com.sliitreg_33.hotelreservationsystem.service.impl;
 
-public class ReservationInfoImpl {
+import com.sliitreg_33.hotelreservationsystem.model.ReservationInfo;
+import com.sliitreg_33.hotelreservationsystem.service.ReservationInfoService;
+
+import java.util.List;
+
+public class ReservationInfoImpl implements ReservationInfoService {
+    @Override
+    public ReservationInfo addReservationInfo(ReservationInfo reservationInfo) {
+        return null;
+    }
+
+    @Override
+    public List<ReservationInfo> getAllReservation() {
+        return null;
+    }
+
+    @Override
+    public ReservationInfo getReservationInfoById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteReservationInfo(int id) {
+
+    }
 }
